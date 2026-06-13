cd811dcdcf79ea7efec474c0d253d706da5dae0c
LUANA ALVES RODRIGUES CAVALCANTI <46127@fiecalunos.lan>
2026-06-12 22:59:19 -0300

    alterações do dia 1206

--- lojaDeRoupas/src/main/java/br/edu/fiec/lojaDeRoupas/model/entity/Cliente.java
+++ lojaDeRoupas/src/main/java/br/edu/fiec/lojaDeRoupas/model/entity/Cliente.java
@@ -1,5 +1,6 @@
 package br.edu.fiec.lojaDeRoupas.model.entity;
 
+import br.edu.fiec.lojaDeRoupas.model.dto.ClienteDTO;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -20,12 +21,21 @@ public class Cliente {
     private String nome;
 
     @Column(nullable = false)
-    private String cpf;
+    private Integer cpf;
 
     @Column(nullable = false)
     private String endereco;
 
     @Column(nullable = false)
     private String email;
+
+
+    // Construtor a partir do DTO
+    public Cliente(ClienteDTO clienteDTO) {
+        setNome(clienteDTO.getNome());
+        setCpf(clienteDTO.getCpf());
+        setEmail(clienteDTO.getEmail());
+        setEndereco(clienteDTO.getEndereco());
+    }
 
 }
--- lojaDeRoupas/src/main/java/br/edu/fiec/lojaDeRoupas/service/ClienteService.java
+++ lojaDeRoupas/src/main/java/br/edu/fiec/lojaDeRoupas/service/ClienteService.java
@@ -1,13 +1,54 @@
 package br.edu.fiec.lojaDeRoupas.service;
 
+import br.edu.fiec.lojaDeRoupas.model.dto.ClienteDTO;
+import br.edu.fiec.lojaDeRoupas.model.entity.Cliente;
 import br.edu.fiec.lojaDeRoupas.repository.ClienteRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class ClienteService {
 
     private ClienteRepository clienteRepository;
+
+    public void cadastrarCliente(ClienteDTO clienteDTO) {
+        clienteRepository.save(new Cliente(clienteDTO));
+    }
+
+    public List<Cliente> buscarTodos() {
+        return clienteRepository.findAll();
+    }
+
+    public Cliente buscarPorId(Integer id) {
+        return clienteRepository.findById(id).orElse(null);
+    }
+
+    public Cliente buscarPorCpf(String cpf) {
+        return clienteRepository.findAllByCpf(cpf).orElse(null);
+    }
+
+    public Cliente buscarPorEmail(String email) {
+        return clienteRepository.findAllByEmail(email).orElse(null);
+    }
+
+    public void apagar(Integer id) {
+        clienteRepository.deleteById(id);
+    }
+
+    public void atualizar(Integer id, ClienteDTO clienteDTO) {
+        Cliente cliente = clienteRepository.findById(id).orElse(null);
+
+        if (cliente != null) {
+            cliente.setNome(clienteDTO.getNome());
+            cliente.setCpf(clienteDTO.getCpf());
+            cliente.setEmail(clienteDTO.getEmail());
+            cliente.setEndereco(clienteDTO.getEndereco());
+
+            clienteRepository.save(cliente);
+        }
+    }
 
 }
